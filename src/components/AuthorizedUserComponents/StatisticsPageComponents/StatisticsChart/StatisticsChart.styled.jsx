99b7ede2c6a9833e9bdcfc9d4d72e4642f53d452
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-08-24 19:37:59 +0300

    add recomendation to statistics styles

---
+++ src/components/AuthorizedUserComponents/StatisticsPageComponents/StatisticsChart/StatisticsChart.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// const gradientColorByDay = '#FFDBD2';
+// const gradientColorByMonth = '#3E85F3';
+
+// Щоб отримати градієнт для колонок статистики як на фото візьми це і розкоміть змінні :)
+// background: linear-gradient(0deg, ${gradientColorByMonth}, #ffffff);
+
+const Container = styled.div``;
+
+export { Container };
