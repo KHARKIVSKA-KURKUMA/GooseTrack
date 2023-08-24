0a1bc8642c72f0e167fade9525f31443f788984d
Olena <anyanyme@gmail.com>
2023-08-24 19:00:02 +0200

    add AuthSection
    AuthSection styled for mobile

--- src/components/UnauthorizedUserComponents/MainPageComponents/AuthSection/AuthSection.jsx
+++ src/components/UnauthorizedUserComponents/MainPageComponents/AuthSection/AuthSection.jsx
@@ -1,7 +1,55 @@
-import { Container } from './AuthSection.styled';
+import {
+  Container,
+  StyledHero,
+  StyledImg,
+  HeroTitle,
+  HeroTitleSpan,
+  AuthBlock,
+  LoginLink,
+  SignupLink,
+} from './AuthSection.styled';
 
-const AuthSection = () => {
-  return <Container>AuthSection</Container>;
+import logoMobile from '../../../../img/mobile/goose1x.png';
+import logoMobile2x from '../../../../img/mobile/goose2x.png';
+import logoMobile3x from '../../../../img/mobile/goose3x.png';
+
+import logoTablet from '../../../../img/tablet/goose1x.png';
+import logoTablet2x from '../../../../img/tablet/goose2x.png';
+import logoTablet3x from '../../../../img/tablet/goose3x.png';
+
+import logo from '../../../../img/desktop/goose1x.png';
+import logo2x from '../../../../img/desktop/goose2x.png';
+import logo3x from '../../../../img/desktop/goose3x.png';
+
+const AuthSection = ({ isHomePage }) => {
+  return (
+    <Container>
+      <StyledHero>
+        <StyledImg>
+          <source
+            media="(max-width: 767px)"
+            srcSet={`${logoMobile} 1x, ${logoMobile2x} 2x, ${logoMobile3x} 3x`}
+          />
+          <source
+            media="(min-width: 768px) and (max-width: 1439px)"
+            srcSet={`${logoTablet} 1x, ${logoTablet2x} 2x, ${logoTablet3x} 3x`}
+          />
+          <source
+            media="(min-width: 1440px)"
+            srcSet={`${logo} 1x, ${logo2x} 2x, ${logo3x} 3x`}
+          />
+          <img src={logoMobile} alt="Logo goose" />
+        </StyledImg>
+        <HeroTitle>
+          G<HeroTitleSpan>oo</HeroTitleSpan>seTrack
+        </HeroTitle>
+        <AuthBlock>
+          <LoginLink>Login</LoginLink>
+          <SignupLink>Sign up</SignupLink>
+        </AuthBlock>
+      </StyledHero>
+    </Container>
+  );
 };
 
 export default AuthSection;
--- src/components/UnauthorizedUserComponents/MainPageComponents/AuthSection/AuthSection.styled.jsx
+++ src/components/UnauthorizedUserComponents/MainPageComponents/AuthSection/AuthSection.styled.jsx
@@ -1,5 +1,131 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const Container = styled.div``;
 
-export { Container };
+const StyledHero = styled.header`
+  position: relative;
+
+  background-color: #3e85f3;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const HeroTitle = styled.h1`
+  color: white;
+  margin: 0;
+  margin-bottom: 32px;
+  font-family: 'Coolvetica';
+  font-size: 44px;
+  line-height: 1.09;
+  font-weight: 700;
+  text-shadow: #00000009;
+
+  @media (min-width: 768px) {
+    
+  }
+`;
+
+const StyledImg = styled.picture`
+  width: 142px;
+  display: block;
+
+  @media (min-width: 768px) {
+   
+  }
+`;
+
+const HeroTitleSpan = styled.span`
+  font-family: 'Coolvetica';
+  font-style: italic;
+  font-size: 44px;
+  line-height: 1.09;
+  font-weight: 700;
+  text-shadow: #00000009;
+
+  @media (min-width: 768px) {
+  }
+`;
+
+
+const AuthBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+
+  @media (min-width: 768px) {
+    
+  }
+`;
+
+const LoginLink = styled(NavLink)`
+  padding: 14px 32px;
+  border-radius: 16px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.3;
+  letter-spacing: -0.02em;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
+    0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+
+  color:  #3e85f3;
+  background-color: white;
+
+  transform: scale(1);
+  transition-property: transform;
+  transition-duration: 250ms;
+  transition-timing-function: cubic-bezier(0, 0.11, 0.35, 2);
+
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+  }
+
+`;
+
+const SignupLink = styled(NavLink)`
+  padding: 14px 32px;
+  border-radius: 16px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.3;
+  letter-spacing: -0.02em;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  text-decoration: underline;
+  text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
+    0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+
+  color: #ffffff;
+  background-color:transparent;
+
+  transform: scale(1);
+  transition-property: transform;
+  transition-duration: 250ms;
+  transition-timing-function: cubic-bezier(0, 0.11, 0.35, 2);
+
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+  }
+`;
+
+export {
+  Container,
+  StyledHero,
+  HeroTitle,
+  StyledImg,
+  HeroTitleSpan,
+  AuthBlock,
+LoginLink,
+  SignupLink
+};
