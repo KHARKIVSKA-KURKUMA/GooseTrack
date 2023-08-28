aa8fdaf66b56571313f8ec657af71916aaf692f9
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-08-28 08:41:16 +0300

    replace Loginpage style

---
+++ src/components/UnauthorizedUserComponents/LoginPageComponents/LoginPageStyled.styled.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  /* min-height: 100vh; */
+  background: var(--auth-background-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding-left: 20px;
+  padding-right: 20px;
+  width: 100%;
+  height: 100%;
+`;
+const Section = styled.section`
+  min-height: 100vh;
+  background: var(--auth-background-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  /* display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start; */
+`;
+
+export { Container, Section };
