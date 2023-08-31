ba0bfb8bba1bb94b9397291f5b6aad8c19d1fac8
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-08-31 13:07:11 +0300

    fix thunk

--- src/store/tasks/tasksThunks.js
+++ src/store/tasks/tasksThunks.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import instance from 'store/auth/helpers/instance';
 
 export const getUserTasksThunk = createAsyncThunk(
   'tasks/fetchAll',
   async (_, { rejectWithValue }) => {
     try {
-      const { data } = await axios.get(`api/tasks/`);
+      const { data } = await instance.get(`api/tasks/`);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
