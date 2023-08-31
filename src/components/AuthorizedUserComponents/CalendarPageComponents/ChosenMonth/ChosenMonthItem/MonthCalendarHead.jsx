8057b2920ff69ec46ca6aca9c715758bcb7d91da
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-08-31 16:02:28 +0300

    update calendar

--- src/components/AuthorizedUserComponents/CalendarPageComponents/ChosenMonth/ChosenMonthItem/MonthCalendarHead.jsx
+++ src/components/AuthorizedUserComponents/CalendarPageComponents/ChosenMonth/ChosenMonthItem/MonthCalendarHead.jsx
@@ -1,5 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { CalendarDaysWrapper, CalendarDayWrapper } from '../ChosenMonth.styled';
+
 const MonthCalendarHead = () => {
-  return <div>MonthCalendarHead</div>;
+
+  const [daysOfWeek, setDaysOfWeek] = useState(['M', 'T', 'W', 'T', 'F', 'S', 'S']);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newDaysOfWeek = window.innerWidth >= 768
+        ? ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+        : ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+      setDaysOfWeek(newDaysOfWeek);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <CalendarDaysWrapper>
+      {daysOfWeek.map((day, index) => (
+        <CalendarDayWrapper
+          key={day + index}
+          isSunday={day === 'Sun' || (index === daysOfWeek.length - 1) || (index === daysOfWeek.length - 2)}
+        >
+          {day}
+        </CalendarDayWrapper>
+      ))}
+    </CalendarDaysWrapper>
+  );
 };
 
 export default MonthCalendarHead;
