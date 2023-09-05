87ecc24b4f40097f06c4223a315fc43e7b9880d8
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-09-05 13:31:08 +0300

    update

--- src/components/AuthorizedUserComponents/CalendarPageComponents/ChosenMonth/ChosenMonthItem/CalendarTable.jsx
+++ src/components/AuthorizedUserComponents/CalendarPageComponents/ChosenMonth/ChosenMonthItem/CalendarTable.jsx
@@ -120,7 +120,7 @@ const CalendarTable = props => {
           );
 
           week.push(
-            <CalendarCell key={j} className={isCurrent ? 'current-day' : ''}>
+            <CalendarCell key={j} className={isCurrent ? 'current-day' : ''} to={`/calendar/day/${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}-${day}`}>
               <GridContainer>
                 <DateNumber isCurrent={isCurrent}>{day}</DateNumber>
                 {renderNotes(day) && (
@@ -142,7 +142,6 @@ const CalendarTable = props => {
   return (
     <CalendarTableWrapper
       onClick={e => console.log(e.currentTarget)}
-      style={{ borderCollapse: 'collapse' }}
     >
       <CalendarNumberWrapper>{generateCalendar()}</CalendarNumberWrapper>
     </CalendarTableWrapper>
