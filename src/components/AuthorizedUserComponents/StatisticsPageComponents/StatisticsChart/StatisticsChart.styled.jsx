f9ac02336238c2b78a61508c124e98062a70996d
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-09-06 13:18:58 +0300

    update styles

--- src/components/AuthorizedUserComponents/StatisticsPageComponents/StatisticsChart/StatisticsChart.styled.jsx
+++ src/components/AuthorizedUserComponents/StatisticsPageComponents/StatisticsChart/StatisticsChart.styled.jsx
@@ -5,19 +5,16 @@ const ChartContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 40px;
-  transform: translateX(20px);
   padding: 45px 15px;
   border: 1px solid #e3f3ff;
   border-radius: 20px;
 
   @media screen and (min-width: 768px) {
     padding: 35px;
-    transform: translateX(65px);
   }
 
   @media screen and (min-width: 1440px) {
     padding: 45px 40px;
-    transform: translateX(365px);
   }
 `;
 
